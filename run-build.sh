e02796e963b7a614f9f76f88b234defcf8b978b6
korben.dong <ghostyuan@outlook.com>
2026-06-30 13:24:37 +0800

    update build tests

--- run-build.sh
+++ run-build.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-rm -rf build
+set -e
+
+rm -rf build build_cuda
 cmake -B build -DGGML_OPENMP=OFF -DLLAMA_CURL=OFF -DCMAKE_BUILD_TYPE=Debug
 cmake --build build --config Debug -j8
 
-cmake --build . --target ggml-c100 -j$(nproc)
+cmake --build build --target ggml-c100 -j"$(nproc)"
+
+cuda_arch="${CMAKE_CUDA_ARCHITECTURES:-80}"
+cmake -S . -B build_cuda \
+    -DGGML_CUDA=ON \
+    -DLLAMA_CURL=OFF \
+    -DCMAKE_CUDA_ARCHITECTURES="${cuda_arch}" \
+    -DCMAKE_BUILD_TYPE=Release
+cmake --build build_cuda --config Release -j "$(nproc)" --target llama-perplexity
