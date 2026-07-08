45d83f7233608efd543cb24b3d8f76d676fee605
korben.dong <ghostyuan@outlook.com>
2026-07-08 10:53:48 +0800

    update build info

--- run-build.sh
+++ run-build.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-#
-# Run from the repository root through Docker:
-# docker run --rm -v "$PWD:/app" -w /app llama.cpp.sim bash -lc \
-#   'cd ext/llama.cpp && bash run-build.sh'
 
-set -e
-
-rm -rf build build_cuda
+rm -rf build build_cuda build-cuda-c100 log/
 cmake -B build -DGGML_OPENMP=OFF -DLLAMA_CURL=OFF -DCMAKE_BUILD_TYPE=Debug
 cmake --build build --config Debug -j8
 
@@ -19,5 +13,12 @@ cmake -S . -B build_cuda \
     -DCMAKE_CUDA_ARCHITECTURES="${cuda_arch}" \
     -DCMAKE_BUILD_TYPE=Release
 
-docker run --gpus all --rm -v "$PWD:/app" -w /app llama.cpp.sim bash -lc \
-    'cmake --build build_cuda --config Release -j "$(nproc)" --target llama-perplexity'
+cmake --build build_cuda --config Release -j "$(nproc)" --target llama-perplexity
+
+cat <<'EOF'
+
+[NOTE] Run from the repository root through Docker:
+docker run --rm -v "$PWD:/app" -w /app llama.cpp.sim bash -lc \
+  'cd ext/llama.cpp && bash run-build.sh'
+EOF
+exit
